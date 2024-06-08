1f552e39f7d4543c616f1d5d2aa29499c1902c84
prateek <sharma_prateek@outlook.com>
2024-06-08 17:50:59 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, request, jsonify
+import time
+
+app = Flask(__name__)
+
+start_time = time.time()
+
+@app.route('/hello/<name>', methods=['GET'])
+def greet(name):
+    return f"Hello, {name}!"
+
+@app.route('/health', methods=['GET'])
+def health_check():
+    current_time = time.time()
+    uptime_seconds = int(current_time - start_time)
+    uptime = {
+        "days": uptime_seconds // 86400,
+        "hours": (uptime_seconds % 86400) // 3600,
+        "minutes": (uptime_seconds % 3600) // 60,
+        "seconds": uptime_seconds % 60
+    }
+
+    health_status = {
+        "status": "Healthy",
+        "message": "The service is up and running!",
+        "uptime": uptime
+    }
+    return jsonify(health_status), 200
+
+if __name__ == '__main__':
+    app.run(debug=False)
